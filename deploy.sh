a0bce31e72bb909ab4475bcd400dd52ed22c7bdb
thomas-young-2013 <1225646303@qq.com>
2017-04-10 20:08:06 +0800

    add deploy script and fix the play auth problems

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+app_home=`pwd`
+TARGET_SERVER="root@remote:/data/wherehows"
+
+# zip the files
+backend="./backend-service/target/universal/stage/"
+web="./web/target/universal/stage/"
+
+cd ${backend}
+zip -r "backend.zip" *
+scp -r "./backend.zip" ${TARGET_SERVER}/backend/;
+
+cd ${web}
+zip -r "web.zip" *
+scp -r "./web.zip" ${TARGET_SERVER}/web/;
