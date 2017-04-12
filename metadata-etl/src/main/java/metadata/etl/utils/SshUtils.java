85df7b4513f06b98a7dd78ee4ead3db49c8b48f9
thomas-young-2013 <1225646303@qq.com>
2017-04-12 22:47:11 +0800

    add ssh utils

---
+++ metadata-etl/src/main/java/metadata/etl/utils/SshUtils.java
@@ -0,0 +1,53 @@
+package metadata.etl.utils;
+
+import com.jcraft.jsch.ChannelExec;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.Session;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+/**
+ * Created by hadoop on 4/12/17.
+ */
+public class SshUtils {
+    public static String exec(String host, String user, String keyLocation, String command) {
+        String result = "";
+        Session session = null;
+        ChannelExec openChannel = null;
+        try {
+            JSch jsch = new JSch();
+
+            session = jsch.getSession(user, host);
+            jsch.addIdentity(keyLocation);
+            java.util.Properties config = new java.util.Properties();
+            config.put("StrictHostKeyChecking", "no");
+            session.setConfig(config);
+            session.connect();
+
+            openChannel = (ChannelExec) session.openChannel("exec");
+            openChannel.setCommand(command);
+            openChannel.connect();
+
+            InputStream in = openChannel.getInputStream();
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(in));
+            String buf = null;
+            while ((buf = reader.readLine()) != null) {
+                result += " " + buf;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            result = null;
+        } finally {
+            if (openChannel != null && !openChannel.isClosed()) {
+                openChannel.disconnect();
+            }
+            if (session != null && session.isConnected()) {
+                session.disconnect();
+            }
+        }
+        return result;
+    }
+}
