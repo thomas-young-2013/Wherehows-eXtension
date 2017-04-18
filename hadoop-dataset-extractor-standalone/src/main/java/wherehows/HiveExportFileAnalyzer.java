fd7a695adef8d238dd104e616c236e42ba537f96
thomas-young-2013 <1225646303@qq.com>
2017-04-18 10:01:45 +0800

    debug: hive export analyzer imp 3

--- hadoop-dataset-extractor-standalone/src/main/java/wherehows/HiveExportFileAnalyzer.java
+++ hadoop-dataset-extractor-standalone/src/main/java/wherehows/HiveExportFileAnalyzer.java
@@ -45,7 +45,7 @@ public class HiveExportFileAnalyzer extends FileAnalyzer {
         bufferedReader.close();*/
 
         String codec = "file.codec";
-        String schemaString = "{\"fields\": [{\"name\": \"name\", \"type\": \"string\"}, {\"name\": \"age\", \"type\": \"int\"}], \"name\": \"Result\", \"namespace\": \"com.tencent\", \"type\": \"record\", \"uri\": \"hdfs:///project/T405/out/000000_0\"}";
+        String schemaString = "{\"fields\": [{\"name\": \"name\", \"type\": \"string\"}, {\"name\": \"age\", \"type\": \"int\"}], \"name\": \"Result\", \"namespace\": \"com.tencent.thomas\", \"type\": \"record\"";
         String storage = STORAGE_TYPE;
         String abstractPath = targetFilePath.toUri().getPath();
 
