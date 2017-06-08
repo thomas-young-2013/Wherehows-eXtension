7faaae452c398f80091bf365f2d15001dc6cda3d
YueYeShen <993698197@qq.com>
2017-06-08 04:58:07 -0700

    repair bug to delete data web display untimely

--- metadata-etl/src/main/resources/jython/HdfsLoad.py
+++ metadata-etl/src/main/resources/jython/HdfsLoad.py
@@ -30,7 +30,7 @@ class HdfsLoad:
     cursor = self.conn_mysql.cursor()
     load_cmd = '''
         DELETE FROM stg_dict_dataset WHERE db_id = {db_id};
-
+        DELETE FROM dict_dataset WHERE db_id = {db_id};
         LOAD DATA LOCAL INFILE '{source_file}'
         INTO TABLE stg_dict_dataset
         FIELDS TERMINATED BY '\Z' ESCAPED BY '\0'
